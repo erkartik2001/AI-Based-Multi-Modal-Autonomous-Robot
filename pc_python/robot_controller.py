e83feb564ca3cb42ef47ff29449d4b45ee8efa77
erkartik2001 <baderiyakartik24@gmail.com>
2026-05-13 00:21:08 +0530

    send commands

---
+++ pc_python/robot_controller.py
@@ -0,0 +1,11 @@
+import requests
+
+ESP_IP = "192.168.1.13"
+
+while True:
+    cmd = input("Enter your command (F/B/L/R/S): ")
+
+    if cmd.isalpha():
+        cmd = cmd.upper()
+
+    requests.get(f"http://{ESP_IP}/{cmd}")
